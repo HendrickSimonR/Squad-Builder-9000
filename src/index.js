154ee35b239e13a2c0f0a0a60bf0eef55cdba667
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-02-23 23:23:25 -0500

    Refactor index.js

--- src/index.js
+++ src/index.js
@@ -11,63 +11,59 @@ document.addEventListener("DOMContentLoaded", async () => {
   let scores = [];
   let nameAndID = [];
 
-    for (let i = 0; i < playerIDs.length; i++) {      
-      let playerID = playerIDs[i];
-      let playerObj;
-
-      if (i % 1 === 0) {
-        setTimeout(() => {}, 1000000000000);
-        playerObj = await fetchPlayerStats(playerID);
-      } else {
-        playerObj = await fetchPlayerStats(playerID);
-      }
-      
-      if (!playerObj) {
-        continue;
-      }  
-
-      let stats = playerObj.data[0];
-      let playerName;
-      let playerPos;
-      let playerTeam;
-      let playerImg;
-
-      if (players[playerID]) {
-        playerName = players[playerID].name;
-        playerPos = players[playerID].pos;
-        playerTeam = players[playerID].team;
-        playerImg = players[playerID].image;
-        nameAndID.push([playerName, playerID]);
-      }
-
-      let offPoints = stats.pts + (stats.ast * 1.5);
-      let defPoints = (stats.reb * 1.2) + (stats.stl * 3) + (stats.blk * 3);
-      let to = stats.turnover;
-      let fantasyAvg = (offPoints + defPoints - to);
-
-      let playerInfo = {
-        image: playerImg,
-        name: playerName,
-        pos: playerPos,
-        team: playerTeam,
-        stats: { 
-          pts: stats.pts, 
-          ast: stats.ast, 
-          ftm: stats.ftm, 
-          reb: stats.reb, 
-          stl: stats.stl, 
-          blk: stats.blk, 
-          to: stats.turnover, 
-          min: stats.min,
-          games: stats.games_played
-        },
-        avg: fantasyAvg,
-        drafted: false
-      };
-      
-      fullDetails[playerID] = playerInfo;
-      scores.push(fantasyAvg);
+  for (let i = 0; i < playerIDs.length; i++) {      
+    let playerID = playerIDs[i];
+    let playerObj;
+    if (i % 1 === 0) {
+      setTimeout(() => {}, 1000000000000);
+      playerObj = await fetchPlayerStats(playerID);
+    } else {
+      playerObj = await fetchPlayerStats(playerID);
     }
+    
+    if (!playerObj) continue;  
+
+    let stats = playerObj.data[0];
+    let playerName;
+    let playerPos;
+    let playerTeam;
+    let playerImg;
+    if (players[playerID]) {
+      playerName = players[playerID].name;
+      playerPos = players[playerID].pos;
+      playerTeam = players[playerID].team;
+      playerImg = players[playerID].image;
+      nameAndID.push([playerName, playerID]);
+    }
+
+    let offPoints = stats.pts + (stats.ast * 1.5);
+    let defPoints = (stats.reb * 1.2) + (stats.stl * 3) + (stats.blk * 3);
+    let to = stats.turnover;
+    let fantasyAvg = (offPoints + defPoints - to);
+    let playerInfo = {
+      image: playerImg,
+      name: playerName,
+      pos: playerPos,
+      team: playerTeam,
+      stats: { 
+        pts: stats.pts, 
+        ast: stats.ast, 
+        ftm: stats.ftm, 
+        reb: stats.reb, 
+        stl: stats.stl, 
+        blk: stats.blk, 
+        to: stats.turnover, 
+        min: stats.min,
+        games: stats.games_played
+      },
+
+      avg: fantasyAvg,
+      drafted: false
+    };
+    
+    fullDetails[playerID] = playerInfo;
+    scores.push(fantasyAvg);
+  }
 
   let sortedScores = scores.sort().reverse();
   let sortedIDs = sortIDs(sortedScores, playerIDs, fullDetails);
