fece6655a87a3439d5e9ce9b9e562117bba83faa
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-02-23 23:15:58 -0500

    Clean renderTeam js

--- src/scripts/renderTeam.js
+++ src/scripts/renderTeam.js
@@ -1,14 +1,5 @@
-import team from "./userInput";
-import displayPlayers from './displayPlayers'
-
-
 function renderTeam(team) {
   let intro = document.getElementById('intro');
-  let menu = document.getElementById('instructions');
-  let nameEl = document.createElement('h1');
-  let draftView = document.getElementById('draftView');
-  // // intro.style.visibility = 'hidden';
-  // draftViewEl.style.visibility = 'visible';
   let header = document.createElement('h1');
   let teamAvg = 0;
 
@@ -16,16 +7,8 @@ function renderTeam(team) {
     intro.children[i].remove();
   }
 
-  // let draftLogButton = document.createElement('span');
-  // draftLogButton.innerHTML = 'Draft Log';
-  // draftLogButton.setAttribute('id', 'draft-log-button');
-  // intro.appendChild(draftLogButton);
-
   let explainButton = document.getElementById('explain-button');
   explainButton.style.visibility = 'visible';
-  //   explainButton.innerHTML = '?';
-  //   explainButton.setAttribute('id', 'explain-button');
-  //   intro.appendChild(explainButton);
 
   let container = document.createElement('div');
     container.setAttribute('id', 'squad-container');
@@ -37,7 +20,6 @@ function renderTeam(team) {
     let squad = document.createElement('div');
     squad.setAttribute('id', `squad-${i + 1}`);
     squad.setAttribute('class', 'squad-row');
-    // console.log('squad', squad)
     container.appendChild(squad);
   }
 
@@ -54,15 +36,12 @@ function renderTeam(team) {
   for (let i = 0; i < fullTeam.length; i++) {
     let player = fullTeam[i];
 
-    // console.log('player', player)
     if (i < 3) {
       let row = document.getElementById('squad-1');
       teamAvg += addDetails(row, player);
-      // console.log('team avg', teamAvg)
     } else {
       let row = document.getElementById('squad-2');
       teamAvg += addDetails(row, player);
-      // console.log('team avg', teamAvg);
     }
   }
 
@@ -85,7 +64,6 @@ function nameTransformer(name) {
 
 function statline(stats) {
   let selectStats = [];
-  // console.log('stats', stats)
 
   for (let stat in stats) {
     if (stat === 'pts') {
@@ -101,7 +79,6 @@ function statline(stats) {
 }
 
 function addDetails(row, person) {
-  // console.log('details', row, person)
   let avg = 0;
   let player = document.createElement('div');
   player.setAttribute('id', 'player');
@@ -124,7 +101,6 @@ function addDetails(row, person) {
     } else if (person[info] === false) {
       continue;
     } else {
-      // console.log('current info', info)
       if (info === 'image') {
         let playerImg = document.createElement('img');
         playerImg.src = person[info];
@@ -136,15 +112,8 @@ function addDetails(row, person) {
       } else if (info === 'avg') {
         col.innerHTML = `AVG FANTASY SCORE: ${person[info].toFixed(4)}`;
         player.appendChild(col);
-        // let moreInfoButton = document.createElement('button');
-        // moreInfoButton.setAttribute('id', 'more-info');
-        // moreInfoButton.innerHTML = 'More Info'
-        // player.appendChild(moreInfoButton);
-        // console.log('player avg before', avg)
         avg += person[info];
-        // console.log('player avg after', avg)
       } else {
-        let capped = info.toUpperCase();
         col.innerHTML = `${person[info].toUpperCase()}`;
         player.appendChild(col);
       }
@@ -155,43 +124,4 @@ function addDetails(row, person) {
   return avg;
 }
 
-// for (let j = 0; j < display.length; j++) {
-//   let rowEl = document.createElement('div');
-//   let player = display[j];
-
-//   for (let k = 0; k < player.length; k++) {
-//     let info = player[k];
-//     let col = document.createElement('div');
-//     col.innerHTML = info;
-//     rowEl.appendChild(col);
-//   }
-
-//   draftViewEl.appendChild(rowEl);
-// }
-// }
-
 export default renderTeam;
-
-
-
-   // let stats = person[info];
-        // let statPairs = [];
-
-        // for (const [key, value] of Object.entries(stats)) {
-        //   statPairs.push([key, value])
-        // }
-
-        // for (let i = 0 ; i < statPairs.length; i++) {
-        //   if (statPairs[i].includes('pts')) {
-        //     col.innerHTML = `${statPairs[i][0]}: ${statPairs[i][1]}`
-        //     rowEl.appendChild(col);
-        //   } else if (statPairs[i].includes('ast')) {
-        //     col.innerHTML = `${statPairs[i][0]}: ${statPairs[i][1]}`
-        //     rowEl.appendChild(col);
-        //   } else if (statPairs[i].includes('reb')) {
-        //     col.innerHTML = `${statPairs[i][0]}: ${statPairs[i][1]}`
-        //     rowEl.appendChild(col);
-        //   } else {
-        //     continue;
-        //   }
-        // }
