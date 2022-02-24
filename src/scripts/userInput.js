4ace30ef616e5e4fa663dc67b46f86ff1442df67
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2022-02-23 23:20:11 -0500

    userInput

--- src/scripts/userInput.js
+++ src/scripts/userInput.js
@@ -1,14 +1,6 @@
-
-// import fetchPlayerStats from "./scripts/fetchPlayerStats";
-// import players from "./scripts/players";
-// import returnInput from "./scripts/userInput";
-// import playerIDs from "./scripts/playerIDs";
-// import findPlayer from "./scripts/findPlayer";
 import draftTeams from "./draftTeams";
 import renderTeam from "./renderTeam";
 import displayPlayers from "./displayPlayers";
-// // import sortIDs from "./scripts/sortIDs";
-// // import playerVitals from "./scripts/playerVitals"
 
 function returnInput(fullDetails, sortedScores, playerInfo) {
   let userInput = document.getElementById('form');
@@ -16,8 +8,6 @@ function returnInput(fullDetails, sortedScores, playerInfo) {
   userInput.addEventListener('submit', function (e) {
     e.preventDefault();
     
-    // console.log('FUNCTION RUN')
-
     let firstName;
     let lastName;
     let fave = e.target[0].value;
@@ -25,8 +15,6 @@ function returnInput(fullDetails, sortedScores, playerInfo) {
     let order = e.target[2].value;
 
     if (parseInt(order) > parseInt(participants)) {
-      // console.log('order', typeof order, order);
-      // console.log('participants', typeof participants, participants);
       let error = document.getElementById('form-error');
       error.style.display = 'block';
       return;
@@ -34,13 +22,10 @@ function returnInput(fullDetails, sortedScores, playerInfo) {
 
     if (fave.includes(' ')) {
       fave = fave.split(' ');
-      
-      // console.log('LIT', fave)
 
       for (let i = 0; i < fave.length; i++) {
         let name = fave[i]; 
         let capped = name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase();
-        // console.log('capped', capped)
 
         if (i === 0) {
           firstName = capped;
@@ -58,17 +43,12 @@ function returnInput(fullDetails, sortedScores, playerInfo) {
       placement: order 
     };
 
-
-    
-    // console.log('VALUES', userValues);
     let team = draftTeams(fullDetails, userValues, sortedScores, playerInfo);
     displayPlayers(team);
     renderTeam(team);
-    // console.log('team drafted', team);
     return userValues;
   });
 }
-
 
 
 export default returnInput;
