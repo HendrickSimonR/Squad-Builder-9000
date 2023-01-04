82bdef643274eb21c8bfdf0d7b6755566b95fa24
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2023-01-03 21:17:33 -0500

    Begins constructing new function for drafting based on season selected

---
+++ src/scripts/selectSeason.js
@@ -0,0 +1,10 @@
+const selectSeason = season => {
+  let seasonArr = season.split(' - ');
+  let seasonSelected = document.getElementById('select-season');
+
+  seasonSelected.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    
+  });
+}
