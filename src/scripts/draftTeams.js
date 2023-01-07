b8cdabe9e8613e5df7144dd55d3961392a13d55e
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2023-01-06 23:15:46 -0500

    Adds round header for draftLog

--- src/scripts/draftTeams.js
+++ src/scripts/draftTeams.js
@@ -15,57 +15,38 @@ function draftTeams(fullDetails, userInput) {
   allPlayers.sort((a, b) => a['avg'] - b['avg']);
 
   while (count < 5) {
+    if (count + 1 !== 6) drafted.push(`Round ${count + 1}`);
+
     for (let i = 0; i < teams.length; i++) {
+      let currentUser = i === userPlacement;
       team = teams[i];
-      if (checkTeam(team)) continue;
 
-      if (i === userPlacement) {
-        if (favoriteFound === false) {
-          let favePlayer = draftFavorite(favorite, allPlayers);
-          favoriteFound = true;
+      if (teamComplete(team)) continue;
 
-          if (!favePlayer) {
-            playerIdx = draftPlayer(team, allPlayers);
-            let player = allPlayers[playerIdx];
-            pos = player["pos"];
-            team[pos].push(player);
-            allPlayers[playerIdx] = `${player.name} drafted!`;
-            details = extractInfo(i, player, pos);
-            drafted.push(`${details}, user`);           
-          } else {
-            let player = allPlayers[favePlayer];
-            pos = player["pos"];
-            team[pos].push(player);
-            allPlayers[playerIdx] = `${player.name} drafted!`;
-            details = extractInfo(i, player, pos);
-            drafted.push(`${details}, user`);               
-          }
-        } else {
-          playerIdx = draftPlayer(team, allPlayers);
-          let player = allPlayers[playerIdx];  
-          pos = player["pos"];
-          team[pos].push(player);
-          allPlayers[playerIdx] = `${player.name} drafted!`;
-          details = extractInfo(i, player, pos);
-          drafted.push(`${details}, user`);                             
-        }
+      if (currentUser && favorite) {
+        playerIdx = draftPlayer(team, allPlayers, favorite);
+        favorite = null;
       } else {
-        playerIdx = draftPlayer(team, allPlayers);       
-        let player = allPlayers[playerIdx];  
-        pos = player["pos"];
-        team[pos].push(player);
-        allPlayers[playerIdx] = `${player.name} drafted!`;
-        details = extractInfo(i, player, pos);
-        drafted.push(`${details}, user`);                             
+        playerIdx = draftPlayer(team, allPlayers);
       }
+
+      let player = allPlayers[playerIdx];
+      pos = player["pos"];
+      team[pos].push(player);
+      allPlayers[playerIdx] = `${player.name} drafted!`;
+      details = extractInfo(i, player, pos);
+
+      currentUser ? drafted.push(`${details}, user`) : drafted.push(`${details}`);                     
     }
 
     count++;
 
+    if (count + 1 !== 6) drafted.push(`Round ${count + 1}`);
+
     for (let i = teams.length - 1; i >= 0; i--) {
       team = teams[i];
 
-      if (checkTeam(team)) continue;
+      if (teamComplete(team)) continue;
 
       playerIdx = draftPlayer(team, allPlayers);
 
@@ -83,12 +64,13 @@ function draftTeams(fullDetails, userInput) {
   }
   console.log('final count', count)
   console.log(teams)
+  console.log('drafted', drafted)
   draftLog(drafted);
 
   return teams[userPlacement];
 }
 
-const checkTeam = team => {
+const teamComplete = team => {
   team = getTeam(team);
   return team.length === 5 ? true : false;
 }
@@ -115,7 +97,12 @@ function draftFavorite(name, allPlayers) {
   return false;
 }
 
-function draftPlayer(team, allPlayers) {
+function draftPlayer(team, allPlayers, favorite = null) {
+  if (favorite) {
+    let faveFound = draftFavorite(favorite, allPlayers); 
+    if (faveFound !== false) return faveFound;
+  } 
+
   for (let i = allPlayers.length - 1; i >= 0; i--) {
     let player = allPlayers[i];
 
@@ -143,43 +130,50 @@ function draftLog(drafted) {
   let playerDetails = document.getElementById('player-details');
 
   for (let i = 0; i < drafted.length; i++) {
-    let container = document.createElement('div');
-    let item = document.createElement('li');
-    let avg = document.createElement('li');
-    let gap = document.createElement('br');
-    let player = drafted[i]
-    let info = player.split(',');
-    let details = `${info[1]}, ${info[2]}, ${info[3]}`
-    avg.innerHTML = `Fantasy Avg: ${info[4]}`;
+    let player = drafted[i];
 
-    let teamLi = document.createElement('li');
-    let team = `Team ${info[0]}`
-    teamLi.innerHTML = team;
-    
-    if (info.length > 5) {
-      item.innerHTML = details;
-      container.setAttribute('id', 'user-team-container');
-      item.setAttribute('id', 'user-team');
-      teamLi.setAttribute('id', 'user-team');
-      avg.setAttribute('id', 'user-team');
-
-      container.appendChild(teamLi);
-      container.appendChild(item);
-      container.appendChild(avg);
-      draftLog.appendChild(container);
-      draftLog.appendChild(gap);
+    if (player.includes('Round')) {
+      let round = document.createElement('h2');
+      round.innerHTML = player;
+      draftLog.appendChild(round);
     } else {
-      item.innerHTML = details;
-      container.setAttribute('id', 'other-team-container');
-      item.setAttribute('id', 'other-team');
-      teamLi.setAttribute('id', 'other-team');
-      avg.setAttribute('id', 'other-team');
+      let container = document.createElement('div');
+      let item = document.createElement('li');
+      let avg = document.createElement('li');
+      let gap = document.createElement('br');
+      let info = player.split(',');
+      let details = `${info[1]}, ${info[2]}, ${info[3]}`
+      avg.innerHTML = `Fantasy Avg: ${info[4]}`;
 
-      container.appendChild(teamLi);
-      container.appendChild(item);
-      container.appendChild(avg);
-      draftLog.appendChild(container);
-      draftLog.appendChild(gap);
+      let teamLi = document.createElement('li');
+      let team = `Team ${info[0]}`
+      teamLi.innerHTML = team;
+      
+      if (info.length > 5) {
+        item.innerHTML = details;
+        container.setAttribute('id', 'user-team-container');
+        item.setAttribute('id', 'user-team');
+        teamLi.setAttribute('id', 'user-team');
+        avg.setAttribute('id', 'user-team');
+
+        container.appendChild(teamLi);
+        container.appendChild(item);
+        container.appendChild(avg);
+        draftLog.appendChild(container);
+        draftLog.appendChild(gap);
+      } else {
+        item.innerHTML = details;
+        container.setAttribute('id', 'other-team-container');
+        item.setAttribute('id', 'other-team');
+        teamLi.setAttribute('id', 'other-team');
+        avg.setAttribute('id', 'other-team');
+
+        container.appendChild(teamLi);
+        container.appendChild(item);
+        container.appendChild(avg);
+        draftLog.appendChild(container);
+        draftLog.appendChild(gap);
+      }
     }
   }
 
