b1bb0739f298f9cae83cfdf414d00c7b58c9d8ff
Hendrick Simon Ramos <hendricksimonr@gmail.com>
2023-01-10 22:56:17 -0500

    Adapts logic in displayModal function

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,21 +1,23 @@
 function displayModal(modal) { // takes input which modal 
   let id = document.getElementById(`${modal}-modal`);
   let close = document.getElementsByClassName(`close-${modal}`)[0];
-  id.style.display = 'block';
-  document.body.style.height = '100%';
-  document.body.style.overflow = 'hidden';
-  close.addEventListener('click', () => closeModal(id));
-
-  let infoQuestions = document.getElementById('info-modal-questions');
-  let footer = document.getElementById('footer');
-  infoQuestions.appendChild(footer);
+  if (id) {
+    id.style.display = 'block';
+    document.body.style.height = '100%';
+    document.body.style.overflow = 'hidden';
+    close.addEventListener('click', () => closeModal(id, modal));
+    let infoQuestions = document.getElementById('info-modal-questions');
+    let footer = document.getElementById('footer');
+    infoQuestions.appendChild(footer);
+  } 
 }
 
-function closeModal(id) { // takes input which modal
+function closeModal(id, modal) { // takes input which modal
   id.style.display = 'none';
   document.body.style.height = '100%';
   document.body.style.overflowX = 'hidden';
   document.body.style.overflowY = 'scroll';
+  if (modal === 'legends') id.setAttribute('id', 'legends-modal-done');
 }
 
 export { displayModal, closeModal };
